e83e794bbdaaa820299c219e884dba16ace5b487
leeGH0804 <dlrlfgud246@naver.com>
2025-06-02 10:01:11 +0900

    CTF 제작 프로젝트

--- 프로젝트/04_CTF_프로젝트/alibaba/urls.py
+++ 프로젝트/04_CTF_프로젝트/alibaba/urls.py
@@ -1,3 +1,11 @@
 
 # /root/ctf-web/myctf/alibaba/urls.py
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_view, name='alibaba_main'),
+    path('door/', views.main_view, name='door'),
+    path("api/access", views.access_api, name="access_api"),
+]
