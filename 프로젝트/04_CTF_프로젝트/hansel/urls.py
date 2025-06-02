17b2f769f8d559b4f156c2c369ec1b7670f7a99a
leeGH0804 <dlrlfgud246@naver.com>
2025-06-02 09:45:15 +0900

    CTF 제작 프로젝트

---
+++ 프로젝트/04_CTF_프로젝트/hansel/urls.py
@@ -0,0 +1,10 @@
+
+# /root/ctf-web/myctf/hansel/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
